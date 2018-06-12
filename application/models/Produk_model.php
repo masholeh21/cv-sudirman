41b3890b84436b38ecaeae286bd2f59368569af2
Your Name <you@example.com>
2018-06-13 03:38:49 +0700

    pagination

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -7,6 +7,9 @@
             $this->load->database();
         }
 
+        public function total_rows($table){
+            return $this->db->count_all_results($table);
+	    }
         // Listing IndustriBesar
         public function listIndustriBesar() {
             $this->db->select('*');
@@ -15,6 +18,28 @@
             $query = $this->db->get();
             return $query->result_array();
         }
+
+        public function read($table,$order,$limit,$offset){
+		// $query = $this->db->query("select * from $table order by ID DESC");
+
+		$this->db->from($table);
+		$this->db->limit($limit,$offset);
+		$this->db->order_by($order, 'ASC');
+
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			foreach($query->result_array() as $row){
+				$data[] = $row;
+			}
+
+			$query->free_result();
+		}
+		else{
+			$data = NULL;
+		}
+
+		return $data;
+	}
 
         // Create IndustriBesar
         public function createIndustriBesar($data) {
--- application/views/front/product/product_1.php
+++ application/views/front/product/product_1.php
@@ -41,15 +41,7 @@
             <?php } ?>
 
             <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                <ul class="pagination">
-                    <li class="disabled"><a href="#">&laquo;</a></li>
-                    <li class="active"><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                    <li><a href="#">&raquo;</a></li>
-                </ul>
+                <?php echo $pagination; ?>
             </div>
         </div>
     </div>
